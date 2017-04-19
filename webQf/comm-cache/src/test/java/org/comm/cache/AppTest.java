c851da1635dd983946e8817bb65066557dc6e303
angel <angel>
2017-04-19 19:57:04 +0800

    spring

--- webQf/comm-cache/src/test/java/org/comm/cache/AppTest.java
+++ webQf/comm-cache/src/test/java/org/comm/cache/AppTest.java
@@ -1,38 +1,52 @@
 package org.comm.cache;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Transaction;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
+	
+	//@Test
+	public void test(){
+		Jedis jedis = new Jedis();
+		jedis.set("name", "zhangsan");
+		
+	}
+	
+	//乐观锁
+	//@Test
+	public void  version(){
+		Jedis jedis = new Jedis();
+		jedis.watch("version");
+		Transaction tx = jedis.multi();
+		tx.incr("version");
+		tx.exec();
+	}
+	@Test
+	public void lock(){
+		Jedis jedis = new Jedis();
+		Long lock  =jedis.setnx("lock","lock");
+		if(lock==1){
+			System.out.println("得到锁");
+	     try{
+	    	 buss();
+	     }finally{
+	    	Long res =  jedis.del("lock");
+	    	if(res==1){
+	    		System.out.println("释放锁成功");
+	    	}
+	     }
+		}else{
+			System.out.println("没有得到锁");
+		}
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	private void buss() {
+		// TODO Auto-generated method stub
+		
+	}
 }
